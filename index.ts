acafbe13b7205cf25fd4974fdc25b1f885518bfb
Alei El-Din <aleiosama@gmail.com>
2022-05-30 21:53:21 +0200

    verification and conf hopefully done

--- index.ts
+++ index.ts
@@ -4,60 +4,116 @@ import { verify } from 'node:crypto';
 import {personalInfo, visitInfo, Doctor, Transaction, gender, bloodType, Block, chain} from './class_file'
 import * as readline from 'node:readline';
 import { stdin, stdout } from 'process';
+
+
+
+// let x = unSerializeChain()
+
+// console.log("X Size")
+// var count = Object.keys(x).length;
+// console.log(count);
+
+// serializeChain()
+
+// x = unSerializeChain()
+
+
+// console.log("X Size")
+// var count = Object.keys(x).length;
+// console.log(count);
+
 //patient 1
 const patient = new personalInfo("patient 1", 20, gender["male"], bloodType["X"], 80, 180, 50, 120, 60)
+const visitTest = new visitInfo(patient, new Date(), "Hasan", "Want to suicide", "Do it")
+const doctor_test = new Doctor("Doctor_test", 50)
+const doctor_4 = new Doctor("Doctor_4", 150)
 // console.log(typeof patient.toString()) //string
 
 const patient_data = patient.toString()
-const blockchain_key = randomBytes(32)
-const blockchain_iv = randomBytes(16)
+const visit_data = visitTest.toString()
 
-// patient cipher 
+// chain.instance.createBlock(patient_data, doctor_test)
+chain.instance.createBlock(visit_data, doctor_test)
 
-const patient_cipher = createCipheriv('aes256', blockchain_key, blockchain_iv)
+// get the transaction data from the block
 
-//patient encryption
+let temp = chain.instance.getBlock(1).getTransaction();
+var temp2 = JSON.parse(temp as string);
+const currTransaction = new Transaction(temp2.transaction, temp2.signiture);
+//console.log(temp2);
+//Object.assign(currTransaction, temp2);
 
-const encrypted_info = patient_cipher.update(patient_data, 'utf8', 'hex') + patient_cipher.final('hex')
-// console.log(`Encrypted Patient data: ${encrypted_info}`)
+console.log(currTransaction.transaction);
 
-//decryption of data
+doctor_test.verify_signiture(currTransaction.signiture, currTransaction.transaction)
+doctor_4.verify_signiture(currTransaction.signiture, currTransaction.transaction)
 
-const decipher_function = createDecipheriv('aes256', blockchain_key, blockchain_iv)
-const decrypted_data = decipher_function.update(encrypted_info, 'hex', 'utf8') + decipher_function.final('utf8')
+
+// checks if patientInfo field is not null/undefined if so then it is a visitInfo record
+var test = JSON.parse(currTransaction.transaction as string);
+//console.log(!!test.patientInfo)
+
+
+
+
+
+
+
+// console.log(chain.instance.getBlock(0).hash)
+
+// for(let i = 0;i < chain.instance.blockchain.length;i++){
+//   console.log(chain.instance.getBlock(i).prevHash)
+// }
+// console.log(chain.instance.getBlock(2,"password"))
+// const blockchain_key = randomBytes(32)
+// const blockchain_iv = randomBytes(16)
+
+// // patient cipher 
+
+// const patient_cipher = createCipheriv('aes256', blockchain_key, blockchain_iv)
+
+// //patient encryption
+
+// const encrypted_info = patient_cipher.update(patient_data, 'utf8', 'hex') + patient_cipher.final('hex')
+// // console.log(`Encrypted Patient data: ${encrypted_info}`)
+
+// //decryption of data
+
+// const decipher_function = createDecipheriv('aes256', blockchain_key, blockchain_iv)
+// const decrypted_data = decipher_function.update(encrypted_info, 'hex', 'utf8') + decipher_function.final('utf8')
 // console.log(`Dencrypted Patient data: ${decrypted_data}`)
 
-const patient_2 = new personalInfo()
-let temp = JSON.parse(decrypted_data)
-Object.assign(patient_2, temp);
+// const patient_2 = new personalInfo()
+//let temp = JSON.parse(decrypted_data)
+//Object.assign(patient_2, temp);
 // console.log(patient_2)
 
 
 
-const doctor_1 = new Doctor("Doctor_1", 50)
-// console.log(doctor_1.publicKey)
+// const doctor_1 = new Doctor("Doctor_1", 50)
+// const doctor_2 = new Doctor("doctor_2", 80 ,"password2")
+// // console.log(doctor_1.publicKey)
 
-const signature = doctor_1.sign("password", patient_data)
-var transaction:Transaction;
-transaction = new Transaction(patient_data, signature)
-const doctor_2 = new Doctor("doctor_2", 80 ,"password2")
-var signature_test:Buffer = Buffer.alloc(0)
-doctor_1.verify_signiture(signature_test, transaction.transaction)
-// console.log(transaction.transaction)
+// const signature = doctor_2.sign("password2", patient_data)
+// var transaction:Transaction;
+// transaction = new Transaction(patient_data, signature)
+// var signature_test:Buffer = Buffer.alloc(0)
+// doctor_1.verify_signiture(signature, transaction.transaction)
+//console.log(transaction.transaction)
 
 
-chain.instance.addBlock(new Block(chain.instance.lastBlock.lastHash, encrypted_info))
+// chain.instance.addBlock(new Block(chain.instance.lastBlock.lastHash, encrypted_info))
 
-console.log(chain.instance)
+// console.log(chain.instance)
 
-console.log(chain.instance.lastBlock.getTransaction("password"))
+// console.log(chain.instance.lastBlock.getTransaction("password"))
 
-var temp2 = chain.instance.lastBlock.getTransaction("password")
+//var temp2 = chain.instance.lastBlock.getTransaction()
 
-const decipher_function_2 = createDecipheriv('aes256', blockchain_key, blockchain_iv)
-const decrypted_data2 = decipher_function_2.update(temp2, 'hex', 'utf8') + decipher_function_2.final('utf8')
+// const decipher_function_2 = createDecipheriv('aes256', blockchain_key, blockchain_iv)
+// const decrypted_data2 = decipher_function_2.update(temp2, 'hex', 'utf8') + decipher_function_2.final('utf8')
 
-console.log(decrypted_data2)
+//console.log(decrypted_data2)
 
 
 
